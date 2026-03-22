51fcf136a2990aea558241b2dc3e65a8d14be900
ThePerkinrex <theperkinrex@gmail.com>
2026-03-22 19:46:51 +0100

    a

--- service/src/api/route.rs
+++ service/src/api/route.rs
@@ -20,38 +20,57 @@ struct RouteRequest {
 }
 
 #[derive(Debug, Deserialize)]
-struct OSRMTrip {
+struct OSMRLeg {
+    duration: f64,
+    distance: f64,
+    annotation: OSMRAnnotation
+}
+
+#[derive(Debug, Deserialize)]
+struct OSMRAnnotation {
+    distance: Vec<f64>,
+    duration: Vec<f64>
+}
+
+
+#[derive(Debug, Deserialize)]
+struct OSRMRoute {
     geometry: String,
     duration: f64,
     distance: f64,
+    legs: Vec<OSMRLeg>
 }
 
 #[derive(Debug, Deserialize)]
 struct RouteOSRMResponse {
-    trips: Vec<OSRMTrip>,
+    routes: Vec<OSRMRoute>,
 }
 
 async fn forward_route(waypoints: &[(f64, f64)]) -> Result<Vec<LineString<f64>>, RouteError> {
     const BASE_URI: &str = "http://router.project-osrm.org/route/v1/driving/";
     let url = format!(
-        "{BASE_URI}{}",
+        "{BASE_URI}{}?annotations=distance,duration",
         waypoints
             .iter()
             .map(|(lat, lon)| format!("{lat},{lon}"))
             .collect::<Vec<_>>()
             .join(";")
     );
-    let resp: String = reqwest::get(url).await?.text().await?;
+    tracing::info!("URL: {url}");
 
-    tracing::info!("Trips: {resp:#?}");
+    // tracing::info!("{}", reqwest::get(&url).await?.text().await?);
+    
+    let resp: RouteOSRMResponse = reqwest::get(url).await?.json().await?;
 
-    todo!()
+    // tracing::info!("Trips: {resp:#?}");
 
-    // Ok(resp
-    //     .trips
-    //     .into_iter()
-    //     .map(|x| polyline::decode_polyline(&x.geometry, 5))
-    //     .collect::<Result<Vec<_>, PolylineError>>()?)
+
+
+    Ok(resp
+        .routes
+        .into_iter()
+        .map(|x| polyline::decode_polyline(&x.geometry, 5))
+        .collect::<Result<Vec<_>, PolylineError>>()?)
 }
 
 async fn get_route(Json(req): Json<RouteRequest>) -> Result<Json<Vec<LineString<f64>>>, AppError> {
